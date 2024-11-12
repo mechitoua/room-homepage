4b3174feb6f48d44f56779b11eabda04eae91560
mehmet_emin <mechitoua.m.amine@outlook.sa>
2024-11-12 20:05:41 +0100

    fix: correctiong the placement of the slider buttons on mobile and desktop layouts

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,41 +1,21 @@
+import { slides } from '@/data';
 import { FC } from 'react';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
-import { Autoplay, Navigation, Pagination } from 'swiper/modules';
+import { Autoplay, Navigation } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
-
-const slides = [
-  {
-    image: '/desktop-image-hero-1.jpg',
-    title: 'Discover innovative ways to decorate',
-    description:
-      'We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love.',
-  },
-  {
-    image: '/desktop-image-hero-2.jpg',
-    title: 'We are available all across the globe',
-    description:
-      "With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we're in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today.",
-  },
-  {
-    image: '/desktop-image-hero-3.jpg',
-    title: 'Manufactured with the best materials',
-    description:
-      'Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office.',
-  },
-];
 
 const Slider: FC = () => {
   return (
     <div className='h-screen md:h-[70vh] relative'>
       <Swiper
-        modules={[Navigation, Pagination, Autoplay]}
+        modules={[Navigation, Autoplay]}
         navigation={{
           nextEl: '.swiper-button-next',
           prevEl: '.swiper-button-prev',
         }}
-        pagination={{ clickable: true }}
+        pagination={{ clickable: false }}
         autoplay={{ delay: 5000 }}
         loop={true}
         className='h-full'>
@@ -76,7 +56,7 @@ const Slider: FC = () => {
           </SwiperSlide>
         ))}
       </Swiper>
-      <div className='hidden md:flex absolute bottom-0 md:left-auto md:right-[41.666667%] z-10'>
+      <div className='hidden left-[58.3333%] bottom-10 right-auto md:flex absolute md:bottom-0 md:left-[58.3333%] md:right-auto z-10'>
         <button className='swiper-button-prev w-24 h-24 bg-black hover:bg-very-dark-gray transition-colors !static flex items-center justify-center'></button>
         <button className='swiper-button-next w-24 h-24 bg-black hover:bg-very-dark-gray transition-colors !static flex items-center justify-center'></button>
       </div>
