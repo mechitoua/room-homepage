17fb18bfe444e88ec947141e59f36a104a2cef42
mehmet_emin <mechitoua.m.amine@outlook.sa>
2024-11-12 20:47:47 +0100

    fix: adjusting padding and margings of the slider section in desktop view

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -22,7 +22,7 @@ const Slider: FC = () => {
         {slides.map((slide, index) => (
           <SwiperSlide key={index}>
             <div className='flex flex-col md:grid md:grid-cols-12 h-full'>
-              <div className='h-[60vh] md:h-auto md:col-span-7 relative'>
+              <div className='h-[50vh] md:h-auto md:col-span-7 relative'>
                 <img
                   src={slide.image}
                   alt={slide.title}
@@ -34,16 +34,18 @@ const Slider: FC = () => {
                   <button className='swiper-button-next w-16 h-16 bg-black hover:bg-very-dark-gray transition-colors !static flex items-center justify-center'></button>
                 </div>
               </div>
-              <div className='flex-1 md:col-span-5 flex items-center px-8 py-8 md:px-16 md:py-12 bg-white'>
-                <div className='max-w-xl'>
-                  <h2 className='text-5xl md:text-5xl font-bold mb-6 md:mb-8 leading-tight font-spartan'>
+              <div className='flex-1 md:col-span-5 flex items-start md:items-center mt-8 md:mt-0 px-8 py-8 md:px-20 md:py-12 bg-white'>
+                <div className='max-w-xl md:max-w-screen-2xl'>
+                  <h2 className='text-[2.3rem] md:text-5xl font-bold mb-4 md:mb-4 leading-[1.1] font-spartan'>
                     {slide.title}
                   </h2>
-                  <p className='text-dark-gray mb-8 md:mb-12 leading-snug text-[1.05rem]'>
+                  <p className='text-dark-gray mb-8 md:mb-8 leading-snug text-[1.05rem]'>
                     {slide.description}
                   </p>
-                  <button className='group inline-flex items-center space-x-8 text-very-dark-gray'>
-                    <span className='text-lg tracking-[0.8em]'>SHOP NOW</span>
+                  <button className='group inline-flex items-center space-x-8 md:-mt-12 text-very-dark-gray'>
+                    <span className='text-[0.95rem] text-black hover:text-gray-500 tracking-[0.8em]'>
+                      SHOP NOW
+                    </span>
                     <img
                       src='/icon-arrow.svg'
                       className='group-hover:translate-x-2 transition-transform h-4'
