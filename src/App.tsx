4c6d2e12e6e02dda5cd7326569ec63261e185293
mehmet_emin <mechitoua.m.amine@outlook.sa>
2024-11-14 20:12:05 +0100

    fix: removing main tag

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,11 @@ import Slider from '@components/Slider';
 
 function App() {
   return (
-    <main>
-      <div className='h-full'>
-        <Navbar />
-        <Slider />
-        <AboutSection />
-      </div>
-    </main>
+    <div className='h-full'>
+      <Navbar />
+      <Slider />
+      <AboutSection />
+    </div>
   );
 }
 
