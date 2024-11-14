33faf993004d543ab409c4cd11a7d3dcded231db
mehmet_emin <mechitoua.m.amine@outlook.sa>
2024-11-14 20:08:33 +0100

    fix: handling the process and __dirname not found issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
+import { fileURLToPath } from 'url';
 import { defineConfig } from 'vite';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
